919df2a73525623885c279648340e65aa4238d8d
Ghernandez1991 <49377511+Ghernandez1991@users.noreply.github.com>
2020-01-27 20:33:28 -0600

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,89 +1,68 @@
-// GET is the default method, so we don't need to set it
-fetch('/hello')
-    .then(function (response) {
-        return response.text();
-    }).then(function (text) {
-        console.log('GET response text:');
-        console.log(text); // Print the greeting as text
-    });
 
-// Send the same request
-fetch('/hello')
-    .then(function (response) {
-        return response.json(); // But parse it as JSON this time
+
+
+
+
+
+
+
+
+
+d3.json("/json").then(function (data) {
+    console.log(data)
+});
+
+
+
+
+function buildpie() {
+
+    var selector1 = d3.select("#col-sm1")
+
+
+
+    d3.json("/json").then(function (response) {
+        var words = response.Most_Spoken_Words;
+        var word_counts = response.Count_of_Words;
+
+       
+        var data = [{
+            values: word_counts,
+            labels: words,
+            textinfo: "label+percent",
+            textposition: "inside",
+            type: 'pie'
+        }];
+
+        var layout = {
+            height: 1000,
+            width: 1600
+        };
+
+        var graph = document.getElementById('graph');
+        Plotly.newPlot(graph, data, layout);
+
+
     })
-    .then(function (json) {
-        console.log('GET response as JSON:');
-        console.log(json); // Here’s our JSON object
-    })
 
 
 
-
-
-
-
-
-// var defaulturl1 = `/scrape`;
-
-
-// d3.json(defaulturl1, function(data) {
-//   console.log(data);
-// });
-
-
-
-
-
-
-
-
-    // var selector1 = d3.select("#col-sm")
     
     
     
-    // d3.json(defaulturl1).then(function (response){
-    //     var words = [];
-    //     var word_counts = [];
-        
-    //     for (i = 0; i < response.length; i++) {
-    //       words.push(response[i].Most_Spoke_Words);
-    //       word_counts.push(response[i].Count_Of_Words);
-          
-    //     }
-    
-    
-    
-    
-    
-    
-    // var trace2 = {
-    //       labels: words,
-    //       values: word_counts,
-    //       type: 'pie',
-    //     };
-    //     var data2 = [trace2];
-    //     var layout2 = {
-    //       title: {
-    //         text: "Most Common Words in Last Statement",
-    //         font: {
-    //           family: "Helvetica, sans-serif",
-    //           size: 25,
-    //           color: "#229954"
-    //         }
-    //       }
-    
-    //     }
-    //     Plotly.newPlot("selector1", data2, layout2);
-    //   });
 
 
- 
- 
- 
+
+
+
+
+
+
+
 
     
 
 
+};
 
-
+buildpie();
